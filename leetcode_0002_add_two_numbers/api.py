b5a7095275be0a4e53d3c5c27bc1089a1d6caa3c
Andy Xie <andy.xie.opensource@gmail.com>
2023-08-23 20:12:54 +0800

    api.py: from_list() implementation

--- leetcode_0002_add_two_numbers/api.py
+++ leetcode_0002_add_two_numbers/api.py
@@ -6,6 +6,19 @@ from typing import Optional
 class ListNode:
     """Definition for singly-linked list"""
 
+    @classmethod
+    def from_list(cls, l: list[int]) -> Optional["ListNode"] | None:
+        """Converts Python list to LeetCode list"""
+
+        if not l:
+            return None
+
+        nodes = [ListNode(val) for val in l]
+        for this, next in zip(nodes[:-1], nodes[1:]):
+            this.next = next
+
+        return nodes[0]
+
     def __init__(
         self,
         val: int = 0,
