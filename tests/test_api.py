12c74f838703963983c70210b382b44effee376f
Andy Xie <andy.xie.opensource@gmail.com>
2023-08-23 19:19:26 +0800

    test_api.py: More types :D

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,15 @@ from leetcode_0002_add_two_numbers import api
 
 
 @pytest.mark.parametrize(
-    ["result", ...],
+    ["result", "l1", "l2"],
     (
         [..., ...],
         [..., ...],
-    )
+    ),
 )
-def test_add_two_numbers(result, ...) -> None:
+def test_add_two_numbers(
+    result: api.ListNode, l1: api.ListNode, l2: api.ListNode
+) -> None:
     """Tests solution for problem Add Two Numbers"""
 
-    assert api.add_two_numbers(...) == result
+    assert api.add_two_numbers(l1, l2) == result
